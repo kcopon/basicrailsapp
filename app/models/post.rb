8a22afaa0f87dba9afd022648168047cce7fa540
Kristoffer <justicecopon@gmail.com>
2015-03-22 19:41:09 -0700

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  scope :ordered_by_title, -> (title) 
+  scope :ordered_by_reverse_created_at -> (time) { where(created_at, time)}.reverse!
 end
+git 
