d6a122e4f4c615c7cf01cc86e8cf847f3c446b47
Kristoffer <justicecopon@gmail.com>
2015-03-28 01:33:56 -0700

    Second Commit - Authorization Assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,25 @@
 class PostPolicy < ApplicationPolicy
   class Scope
+    attr_reader :user, :Scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def index
+      @posts = Post.all?
+        authorize @posts
+      if user.admin? || moderator?
+        scope.where(:id => record.id).exists?
+      elsif policy()
+        scope.where(:id => record.id).exists?
+      else
+        false
+      end
+    end
+  end
+
   def index?
     true
   end
