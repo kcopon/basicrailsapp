7a78fbc78f83f8058a32be79c3901731bf02beda
Kristoffer <justicecopon@gmail.com>
2015-03-29 16:18:21 -0700

    Topics and Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Bloccit::Application.routes.draw do
 
   devise_for :users
-  resources :posts
-  resources :topics
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
 
