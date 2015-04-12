503320c2ffa1791776d63d2a510f09b47a925a19
Kristoffer <justicecopon@gmail.com>
2015-04-11 21:13:36 -0700

    April 11 fix Markdown

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,10 +14,12 @@ class PostsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(post_params)
-      authorize @post
+    @post.topic = @topic
+    authorize @post
+    
     if @post.save
       flash[:notice] = "Post was saved"
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -33,10 +35,11 @@ class PostsController < ApplicationController
   def update
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(post_params)
-      authorize @post
+    authorize @post
+    
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again"
       render :edit
