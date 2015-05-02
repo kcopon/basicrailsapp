80935491a68a853bb344edbe2f43e0f863f82e7e
Kristoffer <justicecopon@gmail.com>
2015-05-02 00:14:09 -0700

    functional app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts
-  mount_uploader :avatar, AvatarUploader
+  has_many :comments
+  mount_uploader :image, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Bloccit::Application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
   resources :topics do
-  resources :posts, except: [:index]
+    resources :posts, except: [:index] do 
+      resource :comments, only: [:create, :destroy]
+    end
   end
 
   get 'about' => 'welcome#about'
