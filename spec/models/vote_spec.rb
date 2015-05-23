5f7a17536932c5c4c4c84c844f5c86622958e02c
Kristoffer <justicecopon@gmail.com>
2015-05-23 11:32:39 -0700

    update on voting checkpoint

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Vote do
+  describe "#up_vote?" do
+    it "returns true for an up vote" do
+      v = Vote.new(value: 1)
+      v.up_vote?.should be_true
+    end
+    it "returns false for a down vote" do
+      v = Vote.new(value: -1)
+      v.up_vote?.should be_false
+    end
+  end
+
+  describe "#down_vote?" do
+    it "returns true for a down vote" do
+      v = Vote.new(value: -1)
+      v.down_vote?.should be_true
+    end
+    it "returns false for an up vote" do
+      v = Vote.new(value: 1)
+      v.down_vote?.should be_false
+    end
+  end
 end
