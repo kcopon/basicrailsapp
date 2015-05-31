5b7cc32ded67b25834e568af36ed38ec600a949c
Kristoffer <justicecopon@gmail.com>
2015-05-30 20:57:02 -0700

    final commit for checkpoint

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -7,12 +7,13 @@ describe VotesController do
 
   describe '#up_vote' do
     it "adds an up-vote to the post" do
+      request.env["HTTP_REFERER"] = '/'
       @user = authenticated_user
       @post = associated_post
       sign_in @user
 
       expect {
-        post( :upvote, post_id: @post.id )
+        post( :up_vote, post_id: @post.id )
       }.to change { @post.up_votes }.by 1
     end
   end
