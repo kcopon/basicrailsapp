0850fe2acb6265a765045938a80a61b8f1d32583
Kristoffer <justicecopon@gmail.com>
2015-05-31 22:31:49 -0700

    first commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,60 @@ describe User do
 
   include TestFactories
 
+  before do
+    @post = associated_post
+    @user = authenticated_user
+  end
+
   describe "#favorited(post)" do
-    xit "returns 'nil if the user has not favorited the post" do
+    it "returns 'nil if the user has not favorited the post" do
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns the appropriate favorite if it exists" do
+    end
+
+    it "returns `nil` if the user has favorited another post" do
     end
   end
+end
+
+
+___
+
+require 'rails_helper'
+
+describe Comment do
+
+ include TestFactories
+
+ describe "after_create" do
+
+   before do
+     @post = associated_post
+     @user = authenticated_user(email_favorites: true)
+     @other_user = authenticated_user
+     @comment = Comment.new(body: 'My comment is really great', post: @post, user: @other_user)
+   end
+
+   it "sends an email to users who have favorited the post" do
+     favorite = @user.favorites.create(post: @post)
+
+     allow( FavoriteMailer )
+       .to receive(:new_comment)
+       .with(@user, @post, @comment)
+       .and_return( double(deliver_now: true) )
+
+       expect( FavoriteMailer )
+         .to receive(:new_comment)
+
+     @comment.save
+   end
+
+   it "does not send emails to users who haven't" do
+     expect( FavoriteMailer )
+       .not_to receive(:new_comment)
+
+     @comment.save
+   end
+ end
 end
