51ba445a6a055faf1885bc4f71cb0bcc4d56cf44
Kristoffer <justicecopon@gmail.com>
2015-06-15 01:56:42 -0700

    first commit

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -23,5 +23,27 @@ describe "Visiting profiles" do
        expect( page ).to have_content(@comment.body)
      end
    end
+
+   before do 
+      @user = FactoryGirl.create(:user)
+      login_as(@user, :scope => :user)
+      @post = associated_post()
+      @comment = Comment.create(user: @user, post: @post, body: "A Comment")
+      allow(@comment).to receive(:send_favorite_emails)
+      @comment.save!
+   end
+
+   describe "signed in" do
+
+     it "shows personal profile" do
+      visit user_path(@user)
+       expect(current_path).to eq(user_path(@user))
+
+       expect( page ).to have_content(@user.name)
+       expect( page ).to have_content(@post.title)
+       expect( page ).to have_content(@comment.body)
+     end
+   end
+
  end
 
